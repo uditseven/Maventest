5e8fcbfd55b37cd2682480c2c0fb37642bbbdd41
uditseven <udit.kumar157@gmail.com>
2021-08-19 14:59:11 +0530

    Basics of Junit ends here

--- src/main/java/com/firstapp/MatchUtils.java
+++ src/main/java/com/firstapp/MatchUtils.java
@@ -9,6 +9,10 @@ public class MatchUtils {
     {
         return a / b;
     }
+    public int multiply(int a, int b)
+    {
+        return a * b;
+    }
     public double computeCircleArea(double radius)
     {
         return Math.PI * radius * radius;
--- src/test/java/com/firstapp/MatchUtilsTest.java
+++ src/test/java/com/firstapp/MatchUtilsTest.java
@@ -1,27 +1,35 @@
 package com.firstapp;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assumptions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS) //is stated this static keyword is no more required
 class MatchUtilsTest extends Object {
     MatchUtils mathutils;
-    @BeforeAll
-    static void beforeAllInit(){
+    TestInfo testInfo;
+    TestReporter testReporter;
+   /* @BeforeAll
+    void beforeAllInit(){    //static  keyword before void is removed
         System.out.println("This runs before all..");
-    }
+    } */
     @BeforeEach
-    void init(){
+    void init(TestInfo testInfo, TestReporter testReporter){
+        this.testInfo = testInfo;
+        this.testReporter = testReporter;
         mathutils = new MatchUtils();    //this runs before each test case, hence reduces code redundancy
+        testReporter.publishEntry("Running " + testInfo.getDisplayName() + " with tags " + testInfo.getTags());
     }
     @AfterEach
     void cleanup(){
         System.out.println("Cleaning up....");
     }
           @Test
+          @DisplayName("Testing add method")
+          @RepeatedTest(3)   //repeated test of a specific test; if one of them fails whole method fails
+          @Tag("Math")
         void test(){
              // MatchUtils mathutils= new MatchUtils(); //Repeated: could be avoided
               int expected=2;
@@ -29,6 +37,8 @@ class MatchUtilsTest extends Object {
               assertEquals(expected,actual,"This method should add two numbers");
           }
           @Test
+          @DisplayName("Testing divide method")
+          @Tag("Math")
           void testDivide(){
              // MatchUtils matchUtils=new MatchUtils();
               int expected=1;
@@ -36,10 +46,40 @@ class MatchUtilsTest extends Object {
               assertEquals(expected,actual,"This should divide two numbers.");
               //assertThrows(NullPointerException.class, () -> mathutils.divide(1,0),"Divide by zero should throw exception.");
           }
-          @Test
-        void testComputeCircleRadius()
-          {
-              //MatchUtils matchUtils=new MatchUtils();
-              assertEquals(314.1592653589793,mathutils.computeCircleArea(10),"Should return area of a circle.");
+          @Nested    //used for grouping similar test cases; if any test fails whole class will fail
+
+          class areaCalculate {
+              @Test
+              @DisplayName("Testing area of circle method")
+              @Tag("Circle")
+              void testComputeCircleRadius() {
+                  //MatchUtils matchUtils=new MatchUtils();
+                  assertEquals(314.1592653589793, mathutils.computeCircleArea(10), "Should return area of a circle.");
+              }
+
           }
-}
+          @Test
+          @Disabled
+          @DisplayName("TDD.. This should not run")
+          void testDisabled(){
+        fail("This test is disabled");
+          }
+         /* @Test
+          @DisplayName("Testing working method")
+           void testDivide_1(){
+                  boolean isServerUp = true;
+                  assumeTrue(isServerUp);
+                  assertThrows(NullPointerException.class, () -> mathutils.divide(1,0),"Divide by zero should throw exception.");
+          }  */
+          @Test
+          @DisplayName("Multiply method")  //testing multiple assumptions simultaneously; used when all conditions needs to be passed
+          @Tag("Math")
+          void testMultiply(){
+              testReporter.publishEntry("Running " + testInfo.getDisplayName() + " with tags " + testInfo.getTags());
+          assertAll(
+                  () -> assertEquals(4,mathutils.multiply(2,2)),
+                  () -> assertEquals(0,mathutils.multiply(2,0)),
+                  () -> assertEquals(-2,mathutils.multiply(2,-1))
+          );
+          }
+         }
