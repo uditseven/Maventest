0b76d1f7f3591f541732573febd88007d4697355
uditseven <udit.kumar157@gmail.com>
2021-08-19 10:12:48 +0530

    First Test case added

---
+++ src/main/java/com/firstapp/MatchUtils.java
@@ -0,0 +1,9 @@
+package com.firstapp;
+
+public class MatchUtils {
+    public int add(int a, int b)
+    {
+        return a + b;
+    }
+
+}
---
+++ src/test/java/com/firstapp/MatchUtilsTest.java
@@ -0,0 +1,12 @@
+package com.firstapp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatchUtilsTest extends Object {
+          @Test
+        void test(){
+              System.out.println("This test ran successfully.");
+          }
+}
